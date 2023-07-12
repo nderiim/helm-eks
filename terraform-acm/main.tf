89b4e9db6f1a4528f36cfae897b17a6519f76cb7
Nderim Bunjaku <bunjaku.nderim@hotmail.com>
2023-07-12 20:17:15 +0200

    Adding ACM module

---
+++ terraform-acm/main.tf
@@ -0,0 +1,35 @@
+variable "access_key" {
+  
+}
+
+variable "secret_key" {
+  
+}
+
+data "aws_route53_zone" "name" {
+  name = local.domain
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+locals {
+  domain = "appstellar.training"
+
+}
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name  = "nderim.${local.domain}"
+  zone_id      = data.aws_route53_zone.name.zone_id
+
+  wait_for_validation = true
+
+  tags = {
+    Name = "my-domain.com"
+  }
+}
